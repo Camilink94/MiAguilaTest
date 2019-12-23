cad1fe50fe168c220a79ddfcb4735713849db7ed
Camilink <camilo.chaparro.co@gmail.com>
2019-12-23 02:28:54 -0500

    feat: Setup Koin

--- app/src/main/java/com/camilink/miaguila/injection/AppModule.kt
+++ app/src/main/java/com/camilink/miaguila/injection/AppModule.kt
@@ -1,7 +1,10 @@
 package com.camilink.miaguila.injection
 
+import com.camilink.miaguila.location.LocationRepo
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
-val appModule = module {
-    
+val appModule: Module = module {
+
+    factory { (listener: LocationRepo.LocationListener) -> LocationRepo(listener) }
 }
