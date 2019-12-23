a5ee6cc2511fde15dbb33bdf503abeb309387021
Camilink <camilo.chaparro.co@gmail.com>
2019-12-23 01:33:00 -0500

    feat: Add classes for MVP and Injection

---
+++ app/src/main/java/com/camilink/miaguila/Application.kt
@@ -0,0 +1,7 @@
+package com.camilink.miaguila
+
+import android.app.Application
+
+class MyApplication: Application(){
+
+}
---
+++ app/src/main/java/com/camilink/miaguila/presenter/MapsPresenter.kt
@@ -0,0 +1,5 @@
+package com.camilink.miaguila.presenter
+
+class MapsPresenter {
+    
+}
