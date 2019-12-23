d30e06dd1c79faddd51fb74387e1e919de462a4e
Camilink <camilo.chaparro.co@gmail.com>
2019-12-23 02:44:47 -0500

    feat: Add activity injection

--- app/src/main/java/com/camilink/miaguila/injection/AppModule.kt
+++ app/src/main/java/com/camilink/miaguila/injection/AppModule.kt
@@ -1,10 +1,13 @@
 package com.camilink.miaguila.injection
 
 import com.camilink.miaguila.location.LocationRepo
+import com.camilink.miaguila.presenter.MapsPresenter
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val appModule: Module = module {
 
     factory { (listener: LocationRepo.LocationListener) -> LocationRepo(listener) }
+    factory { (view: MapsPresenter.View) -> MapsPresenter(view) }
+
 }
--- app/src/main/java/com/camilink/miaguila/presenter/MapsPresenter.kt
+++ app/src/main/java/com/camilink/miaguila/presenter/MapsPresenter.kt
@@ -6,10 +6,13 @@ import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.core.parameter.parametersOf
 
-class MapsPresenter(val view: View) : LocationRepo.LocationListener, KoinComponent {
+class MapsPresenter(private val view: View) : LocationRepo.LocationListener, KoinComponent {
 
-    val locationRepo: LocationRepo by inject { parametersOf(this) }
+    private val locationRepo: LocationRepo by inject { parametersOf(this) }
 
+    fun getFirstRoute() {
+        locationRepo.getFirstRoute()
+    }
 
     override fun showFirstRoute(polyOptions: PolylineOptions) {
         view.showFirstRoute(polyOptions)
