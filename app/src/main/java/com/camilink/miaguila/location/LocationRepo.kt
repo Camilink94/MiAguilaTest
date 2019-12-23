cd1c71ab67e9644f21c8a0fe63ac7c5e5ef0eb71
Camilink <camilo.chaparro.co@gmail.com>
2019-12-23 01:48:24 -0500

    feat: Add methods to Location Repo

--- app/src/main/java/com/camilink/miaguila/location/LocationRepo.kt
+++ app/src/main/java/com/camilink/miaguila/location/LocationRepo.kt
@@ -1,5 +1,20 @@
 package com.camilink.miaguila.location
 
-class LocationRepo {
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+
+class LocationRepo(private val listener: LocationListener) {
+
+    fun getFirstRoute() {
+        val routeOptions: PolylineOptions = PolylineOptions()
+            .add(LatLng(4.667426, -74.056624))
+            .add(LatLng(4.672655, -74.054071))
+
+        listener.showFirstRoute(routeOptions)
+    }
+
+    interface LocationListener {
+        fun showFirstRoute(polyOptions: PolylineOptions)
+    }
 
 }
