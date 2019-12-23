8720f95d7d5d4805379b2cf407d0a9dc10fe94a4
Camilink <camilo.chaparro.co@gmail.com>
2019-12-23 02:38:52 -0500

    feat: Add presenter injection

--- app/src/main/java/com/camilink/miaguila/presenter/MapsPresenter.kt
+++ app/src/main/java/com/camilink/miaguila/presenter/MapsPresenter.kt
@@ -1,5 +1,21 @@
 package com.camilink.miaguila.presenter
 
-class MapsPresenter {
-    
+import com.camilink.miaguila.location.LocationRepo
+import com.google.android.gms.maps.model.PolylineOptions
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import org.koin.core.parameter.parametersOf
+
+class MapsPresenter(val view: View) : LocationRepo.LocationListener, KoinComponent {
+
+    val locationRepo: LocationRepo by inject { parametersOf(this) }
+
+
+    override fun showFirstRoute(polyOptions: PolylineOptions) {
+        view.showFirstRoute(polyOptions)
+    }
+
+    interface View {
+        fun showFirstRoute(polyOptions: PolylineOptions)
+    }
 }
