507002cb83e121a34d0cc59a2d01032ffc408401
Camilink <camilo.chaparro.co@gmail.com>
2020-01-06 18:05:38 -0500

    feat: Add LocationManager inside Repo
    Changed LocationRepo Listener class name

--- app/src/main/java/com/camilink/miaguila/injection/AppModule.kt
+++ app/src/main/java/com/camilink/miaguila/injection/AppModule.kt
@@ -1,13 +1,19 @@
 package com.camilink.miaguila.injection
 
+import android.content.Context
+import android.location.LocationManager
 import com.camilink.miaguila.location.LocationRepo
 import com.camilink.miaguila.presenter.MapsPresenter
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val appModule: Module = module {
 
-    factory { (listener: LocationRepo.LocationListener) -> LocationRepo(listener) }
+    factory { (listener: LocationRepo.LocationRepoListener) -> LocationRepo(listener) }
     factory { (view: MapsPresenter.View) -> MapsPresenter(view) }
+    factory { androidApplication() }
+    factory { androidContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager }
 
 }
--- app/src/main/java/com/camilink/miaguila/location/LocationRepo.kt
+++ app/src/main/java/com/camilink/miaguila/location/LocationRepo.kt
@@ -1,9 +1,23 @@
 package com.camilink.miaguila.location
 
+import android.annotation.SuppressLint
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PolylineOptions
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class LocationRepo(private val listener: LocationListener) {
+class LocationRepo(private val listener: LocationRepoListener) : LocationListener, KoinComponent {
+
+    private val locationManager: LocationManager by inject()
+
+    @SuppressLint("MissingPermission")
+    fun startLocationService() {
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f,this)
+    }
 
     fun getFirstRoute() {
         val routeOptions: PolylineOptions = PolylineOptions()
@@ -13,7 +27,25 @@ class LocationRepo(private val listener: LocationListener) {
         listener.showFirstRoute(routeOptions)
     }
 
-    interface LocationListener {
+    //region LocationListener
+    override fun onLocationChanged(location: Location?) {
+
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+
+    }
+    //endregion
+
+    interface LocationRepoListener {
         fun showFirstRoute(polyOptions: PolylineOptions)
     }
 
